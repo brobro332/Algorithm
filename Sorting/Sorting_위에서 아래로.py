9de02cbe2ead6166510210652df4662dd177b6b8
brobro332 <93824318+brobro332@users.noreply.github.com>
2023-02-21 00:37:23 +0900

    6th commit

--- Sorting/Sorting_위에서 아래로.py
+++ Sorting/Sorting_위에서 아래로.py
@@ -1,23 +1,26 @@
-# 수열에 속한 수의 개수 N이 주어질 때 수열을 내림차순으로 정렬하시오.
+# N명의 학생 정보가 있다. 
 
-# 1 <= N <= 500
+# 각 학생의 이름과 성적 정보가 주어졌을 때 성적이 낮은 순으로 학생 이름을
 
-# 수의 범위는 1 이상 100,000 이하의 자연수이다.
+# 출력하시오.
+
+# 1 <= N <= 100,000
 
 array = []
 n = int(input())
 
 for i in range(n) :
-    array.append(int(input()))
+    a = input().split()
+    array.append((a[0], a[1]))
 
 import time
-start_time = time.time()    
-    
-array = sorted(array, reverse=True)
+start_time = time.time()
 
-for i in range(len(array)) : 
-    print(array[i], end=' ')
+array = sorted(array, key=lambda name : name[1])
+
+for name in array :
+    print(name[0], end=' ')
 print()
 end_time = time.time()
-print("time:", end_time - start_time)
+print("time:",end_time - start_time)
 print()
